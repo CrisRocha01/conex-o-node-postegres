9195ebf0162019ed4e52a88b2709b0e48d19d2e4
CrisRocha01 <cristiano_g_rocha@yahoo.com.br>
2024-10-24 22:35:57 -0300

    centralizacao dos tratamentos de erro video 12

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,11 @@
 import fastify from 'fastify'
 import { personRoutes } from '@/http/controllers/person/routes'
 import { userRoutes } from '@/http/controllers/user/routes'
-import { ZodError } from 'zod'
-import { env } from './env'
+import { globalErroHandler } from '@/utils/global-error-handler'
 
 export const app = fastify()
 
 app.register(personRoutes)
 app.register(userRoutes)
 
-app.setErrorHandler((error, _, reply) => {
-  if (error instanceof ZodError) {
-    return reply
-      .status(400)
-      .send({ message: 'Validadtion Erro', errors: error.format() })
-  }
-
-  if (env.NODE_ENV === 'development') {
-    console.error(error)
-  }
-
-  return reply.status(500).send({ message: 'Internal server error' })
-})
+app.setErrorHandler(globalErroHandler)
